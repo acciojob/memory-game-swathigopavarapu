9ff946e4ff69e1c0ce9e28afaefd0053b02945bc
Swathi Gopavarapu <swathigopavarapu1@gmail.com>
2025-09-25 16:30:56 +0530

    initial commit : memory

--- src/components/LevelSelector.js
+++ src/components/LevelSelector.js
@@ -3,12 +3,18 @@ import React from "react";
 const LevelSelector = ({ setLevel }) => {
   return (
     <div className="levels_container">
-      <h2>Select Difficulty</h2>
-      <div>
-        <button id="easy" onClick={() => setLevel('easy')}>Easy</button>
-        <button id="normal" onClick={() => setLevel('normal')}>Normal</button>
-        <button id="hard" onClick={() => setLevel('hard')}>Hard</button>
-      </div>
+      <label>
+        <input type="radio" id="easy" name="level" onChange={() => setLevel('easy')} />
+        Easy
+      </label>
+      <label>
+        <input type="radio" id="normal" name="level" onChange={() => setLevel('normal')} />
+        Normal
+      </label>
+      <label>
+        <input type="radio" id="hard" name="level" onChange={() => setLevel('hard')} />
+        Hard
+      </label>
     </div>
   );
 };
